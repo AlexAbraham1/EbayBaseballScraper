d5e236f99efc931f40625b3b564cae0fc438d4b4
Alex Abraham <alex.abraham@vimeo.com>
2023-06-03 23:16:10 -0400

    initial work

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # EbayBaseballScraper
 Using scrapy to pull baseball card sell information from ebay
+
+## How to Run
+```shell
+scrapy runspider ebay_spider.py -o output.json
+```
---
+++ ebay_spider.py
@@ -0,0 +1,30 @@
+import scrapy
+
+class EbaySpider(scrapy.Spider):
+    name = "ebay_spider"
+    start_urls = ["https://www.ebay.com/sch/i.html?_from=R40&_nkw=baseball+card&_sacat=0&rt=nc&LH_Sold=1&LH_Complete=1"]
+
+    def parse(self, response):
+        html_blocks = response.xpath("//li[starts-with(@id, 'item')]")
+        for html_block in html_blocks:
+            item = {}
+
+            item["date_sold"] = html_block.xpath(".//div[@class='s-item__caption-section']//text()").get()[6:]
+            item['name'] = html_block.xpath(".//a[@class='s-item__link']//span[@role='heading']//text()").get()
+            item['link'] = html_block.xpath(".//a[@class='s-item__link']/@href").get()
+            item['condition'] = html_block.xpath(".//span[@class='SECONDARY_INFO']//text()").get()
+            item['price'] = html_block.xpath(".//span[@class='s-item__price']//text()").get()
+            item['num_bids'] = html_block.xpath(".//span[contains(@class, 's-item__bidCount')]//text()").get()
+            item['shipping'] = html_block.xpath(".//span[contains(@class, 's-item__shipping')]//text()").get()
+
+            seller_info = item['seller_username'] = html_block.xpath(".//span[@class='s-item__seller-info-text']//text()").get().split(" ")
+            item['seller_username'] = seller_info[0]
+            item['seller_num_reviews'] = seller_info[1][1:-1]
+            item['seller_pct_positive_feedback'] = seller_info[2]
+
+            # Parse through 50 pages
+            for pgn in range(2,50):
+                new_url = "https://www.ebay.com/sch/i.html?_from=R40&_nkw=baseball+card&_sacat=0&rt=nc&LH_Sold=1&LH_Complete=1" + f"&_pgn={pgn}"
+                yield scrapy.Request(new_url, callback=self.parse)
+
+            yield item
